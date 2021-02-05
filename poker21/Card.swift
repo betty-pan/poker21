512194000df50d09a7f86b111fc42dc62939e0dd
Betty Pan <betty19931218@gmail.com>
2021-02-05 18:48:29 +0800

    Init

---
+++ poker21/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  poker21
+//
+//  Created by Betty Pan on 2021/1/22.
+//
+
+import Foundation
+
+let suits = ["♣️", "♦️", "♥️", "♠️"]
+let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+class Card {
+    var suit = ""
+    var rank = ""
+
+}
